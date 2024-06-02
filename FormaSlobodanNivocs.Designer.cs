eb9876a13748ed34fa04513d48f8f3f3a5386a20
SofijaaP <104568179+SofijaaP@users.noreply.github.com>
2024-06-02 14:11:13 +0200

    sveska

--- FormaSlobodanNivocs.Designer.cs
+++ FormaSlobodanNivocs.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pnlSveska = new System.Windows.Forms.Panel();
             this.btnSveska = new System.Windows.Forms.Button();
             this.btnObrisi = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -36,30 +37,44 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(185, 36);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "aaa";
             // 
+            // pnlSveska
+            // 
+            this.pnlSveska.Enabled = false;
+            this.pnlSveska.Location = new System.Drawing.Point(460, 64);
+            this.pnlSveska.Name = "pnlSveska";
+            this.pnlSveska.Size = new System.Drawing.Size(200, 100);
+            this.pnlSveska.TabIndex = 1;
+            this.pnlSveska.Visible = false;
+            this.pnlSveska.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlSveska_Paint);
+            this.pnlSveska.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlSveska_MouseDown);
+            this.pnlSveska.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlSveska_MouseMove);
+            this.pnlSveska.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlSveska_MouseUp);
+            // 
             // btnSveska
             // 
-            this.btnSveska.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSveska.Location = new System.Drawing.Point(21, 26);
+            this.btnSveska.Location = new System.Drawing.Point(51, 53);
             this.btnSveska.Name = "btnSveska";
-            this.btnSveska.Size = new System.Drawing.Size(123, 43);
-            this.btnSveska.TabIndex = 1;
+            this.btnSveska.Size = new System.Drawing.Size(75, 23);
+            this.btnSveska.TabIndex = 2;
             this.btnSveska.Text = "SVESKA";
             this.btnSveska.UseVisualStyleBackColor = true;
+            this.btnSveska.Click += new System.EventHandler(this.btnSveska_Click);
             // 
             // btnObrisi
             // 
-            this.btnObrisi.Location = new System.Drawing.Point(52, 495);
+            this.btnObrisi.Location = new System.Drawing.Point(90, 437);
             this.btnObrisi.Name = "btnObrisi";
             this.btnObrisi.Size = new System.Drawing.Size(75, 23);
-            this.btnObrisi.TabIndex = 2;
+            this.btnObrisi.TabIndex = 3;
             this.btnObrisi.Text = "OBRISI";
             this.btnObrisi.UseVisualStyleBackColor = true;
+            this.btnObrisi.Click += new System.EventHandler(this.btnObrisi_Click);
             // 
             // frmSlobodanNivo
             // 
@@ -68,8 +83,9 @@
             this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.btnObrisi);
             this.Controls.Add(this.btnSveska);
+            this.Controls.Add(this.pnlSveska);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmSlobodanNivo";
             this.Text = "FormaSlobodanNivocs";
             this.Load += new System.EventHandler(this.frmSlobodanNivo_Load);
@@ -83,6 +99,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel pnlSveska;
         private System.Windows.Forms.Button btnSveska;
         private System.Windows.Forms.Button btnObrisi;
     }
--- FormaSlobodanNivocs.cs
+++ FormaSlobodanNivocs.cs
@@ -21,16 +21,25 @@ namespace AgeomProj
             indexZadatka = 0;
         }
         public Point gornjiLevi;
-        public int duzinaStr;
+        public int duzinaStr; 
+        int visinaForme;
+        int sirinaForme;
         public Point centar;
         public Nivo<SlobodanZadatak> nivo;
         public int indexZadatka;
+        bool crtanjeUSvesci = false;
+        bool otvorenaSveska = false;
+        Point prethodna;
+        Point novaTacka;
+        Graphics sveska;
         //U OVU STATICKU PROMENLJIVU SE CUVA NAZIV NIVOA KOJI TREBA DA SE UCITA
         //TREBA ONA DA SE AZURIRA PRE NEGO STO SE PRITISNE DUGME ZA ULAZAK U NIVO
         public static string nazivFajlaNivoa;
         private void frmSlobodanNivo_Load(object sender, EventArgs e)
         {
             RadnaPovrsina.IzracunajPolja(this, out gornjiLevi, out centar, out duzinaStr);
+            visinaForme = this.Height;
+            sirinaForme = this.Width;
         }
 
         private void frmSlobodanNivo_Paint(object sender, PaintEventArgs e)
@@ -39,14 +48,86 @@ namespace AgeomProj
             nivo.Zadaci[indexZadatka].Nacrtaj(e.Graphics, centar, duzinaStr / 20);
 
         }
-
+        public void VeLicinaLokacijaSvega()
+        {
+            pnlSveska.Left = 0;
+            pnlSveska.Top = 0;
+            pnlSveska.Width = sirinaForme;
+            pnlSveska.Height = visinaForme;
+            sveska = pnlSveska.CreateGraphics();
+        }
         private void frmSlobodanNivo_SizeChanged(object sender, EventArgs e)
         {
             if (WindowState != FormWindowState.Minimized)
             {
                 RadnaPovrsina.IzracunajPolja(this, out gornjiLevi, out centar, out duzinaStr);
                 this.Refresh();
+                visinaForme = this.Height;
+                sirinaForme = this.Width;
+                VeLicinaLokacijaSvega();
             }
         }
+
+        private void btnSveska_Click(object sender, EventArgs e)
+        {
+            if (!otvorenaSveska)
+            {
+                pnlSveska.Enabled = true;
+                pnlSveska.Visible = true;
+                btnObrisi.Enabled = true;
+                btnObrisi.Visible = true;
+                otvorenaSveska = true;
+                btnSveska.Text = "NAZAD";
+                sveska = pnlSveska.CreateGraphics();
+            }
+            else
+            {
+                pnlSveska.Enabled = false;
+                pnlSveska.Visible = false;
+                btnObrisi.Enabled = false;
+                btnObrisi.Visible = false;
+                otvorenaSveska = false;
+                btnSveska.Text = "SVESKA";
+            }
+        }
+
+        private void pnlSveska_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i <= pnlSveska.Height; i += duzinaStr / 20)
+            {
+                e.Graphics.DrawLine(Pens.Gray, 0, i, pnlSveska.Width, i);
+            }
+            for (int i = 0; i <= pnlSveska.Width; i += duzinaStr / 20)
+            {
+                e.Graphics.DrawLine(Pens.Gray, i, 0, i, pnlSveska.Height);
+            }
+        }
+
+        private void pnlSveska_MouseDown(object sender, MouseEventArgs e)
+        {
+            crtanjeUSvesci = true;
+            prethodna = e.Location;
+        }
+
+        private void pnlSveska_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (crtanjeUSvesci)
+            {
+                novaTacka = e.Location;
+                sveska.DrawLine(Pens.Black, prethodna, novaTacka);
+                prethodna = novaTacka;
+            }
+        }
+
+        private void pnlSveska_MouseUp(object sender, MouseEventArgs e)
+        {
+            crtanjeUSvesci = false;
+        }
+
+        private void btnObrisi_Click(object sender, EventArgs e)
+        {
+            pnlSveska.Refresh();
+        }
+
     }
 }
