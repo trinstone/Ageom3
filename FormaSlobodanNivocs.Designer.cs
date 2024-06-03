ca33f7f2669dc106a1b55f3ec36a50d1984e3ffd
SofijaaP <104568179+SofijaaP@users.noreply.github.com>
2024-06-03 16:47:18 +0200

    Update FormaSlobodanNivocs.Designer.cs

--- FormaSlobodanNivocs.Designer.cs
+++ FormaSlobodanNivocs.Designer.cs
@@ -53,10 +53,10 @@
             // 
             // btnPosalji
             // 
-            this.btnPosalji.Location = new System.Drawing.Point(16, 416);
-            this.btnPosalji.Margin = new System.Windows.Forms.Padding(2);
+            this.btnPosalji.Location = new System.Drawing.Point(21, 512);
+            this.btnPosalji.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnPosalji.Name = "btnPosalji";
-            this.btnPosalji.Size = new System.Drawing.Size(135, 19);
+            this.btnPosalji.Size = new System.Drawing.Size(180, 23);
             this.btnPosalji.TabIndex = 31;
             this.btnPosalji.Text = "POSALJI ODGOVOR";
             this.btnPosalji.UseVisualStyleBackColor = true;
@@ -66,10 +66,10 @@
             // 
             this.pbxSrce3.BackgroundImage = global::AgeomProj.Properties.Resources.Heart_corazón_svg;
             this.pbxSrce3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pbxSrce3.Location = new System.Drawing.Point(683, 15);
-            this.pbxSrce3.Margin = new System.Windows.Forms.Padding(2);
+            this.pbxSrce3.Location = new System.Drawing.Point(911, 18);
+            this.pbxSrce3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pbxSrce3.Name = "pbxSrce3";
-            this.pbxSrce3.Size = new System.Drawing.Size(29, 29);
+            this.pbxSrce3.Size = new System.Drawing.Size(39, 36);
             this.pbxSrce3.TabIndex = 30;
             this.pbxSrce3.TabStop = false;
             // 
@@ -77,10 +77,10 @@
             // 
             this.pbxSrce2.BackgroundImage = global::AgeomProj.Properties.Resources.Heart_corazón_svg;
             this.pbxSrce2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pbxSrce2.Location = new System.Drawing.Point(717, 15);
-            this.pbxSrce2.Margin = new System.Windows.Forms.Padding(2);
+            this.pbxSrce2.Location = new System.Drawing.Point(956, 18);
+            this.pbxSrce2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pbxSrce2.Name = "pbxSrce2";
-            this.pbxSrce2.Size = new System.Drawing.Size(32, 29);
+            this.pbxSrce2.Size = new System.Drawing.Size(43, 36);
             this.pbxSrce2.TabIndex = 29;
             this.pbxSrce2.TabStop = false;
             // 
@@ -88,29 +88,28 @@
             // 
             this.pbxSrce1.BackgroundImage = global::AgeomProj.Properties.Resources.Heart_corazón_svg;
             this.pbxSrce1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pbxSrce1.Location = new System.Drawing.Point(753, 15);
-            this.pbxSrce1.Margin = new System.Windows.Forms.Padding(2);
+            this.pbxSrce1.Location = new System.Drawing.Point(1004, 18);
+            this.pbxSrce1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pbxSrce1.Name = "pbxSrce1";
-            this.pbxSrce1.Size = new System.Drawing.Size(32, 29);
+            this.pbxSrce1.Size = new System.Drawing.Size(43, 36);
             this.pbxSrce1.TabIndex = 28;
             this.pbxSrce1.TabStop = false;
             // 
             // lblTimer
             // 
             this.lblTimer.AutoSize = true;
-            this.lblTimer.Location = new System.Drawing.Point(38, 91);
-            this.lblTimer.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTimer.Location = new System.Drawing.Point(51, 112);
             this.lblTimer.Name = "lblTimer";
-            this.lblTimer.Size = new System.Drawing.Size(29, 13);
+            this.lblTimer.Size = new System.Drawing.Size(36, 16);
             this.lblTimer.TabIndex = 27;
             this.lblTimer.Text = "timer";
             // 
             // btnPomoc
             // 
-            this.btnPomoc.Location = new System.Drawing.Point(16, 50);
-            this.btnPomoc.Margin = new System.Windows.Forms.Padding(2);
+            this.btnPomoc.Location = new System.Drawing.Point(21, 62);
+            this.btnPomoc.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnPomoc.Name = "btnPomoc";
-            this.btnPomoc.Size = new System.Drawing.Size(80, 28);
+            this.btnPomoc.Size = new System.Drawing.Size(107, 34);
             this.btnPomoc.TabIndex = 26;
             this.btnPomoc.Text = "POMOC";
             this.btnPomoc.UseVisualStyleBackColor = true;
@@ -119,58 +118,55 @@
             // lblOdg3
             // 
             this.lblOdg3.AutoSize = true;
-            this.lblOdg3.Location = new System.Drawing.Point(14, 392);
-            this.lblOdg3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblOdg3.Location = new System.Drawing.Point(19, 482);
             this.lblOdg3.Name = "lblOdg3";
-            this.lblOdg3.Size = new System.Drawing.Size(35, 13);
+            this.lblOdg3.Size = new System.Drawing.Size(44, 16);
             this.lblOdg3.TabIndex = 25;
             this.lblOdg3.Text = "label3";
             // 
             // lblOdg2
             // 
             this.lblOdg2.AutoSize = true;
-            this.lblOdg2.Location = new System.Drawing.Point(14, 370);
-            this.lblOdg2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblOdg2.Location = new System.Drawing.Point(19, 455);
             this.lblOdg2.Name = "lblOdg2";
-            this.lblOdg2.Size = new System.Drawing.Size(35, 13);
+            this.lblOdg2.Size = new System.Drawing.Size(44, 16);
             this.lblOdg2.TabIndex = 24;
             this.lblOdg2.Text = "label2";
             // 
             // lblOdg1
             // 
             this.lblOdg1.AutoSize = true;
-            this.lblOdg1.Location = new System.Drawing.Point(14, 346);
-            this.lblOdg1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblOdg1.Location = new System.Drawing.Point(19, 426);
             this.lblOdg1.Name = "lblOdg1";
-            this.lblOdg1.Size = new System.Drawing.Size(35, 13);
+            this.lblOdg1.Size = new System.Drawing.Size(44, 16);
             this.lblOdg1.TabIndex = 23;
             this.lblOdg1.Text = "label1";
             // 
             // tbxOdg3
             // 
-            this.tbxOdg3.Location = new System.Drawing.Point(76, 390);
-            this.tbxOdg3.Margin = new System.Windows.Forms.Padding(2);
+            this.tbxOdg3.Location = new System.Drawing.Point(101, 480);
+            this.tbxOdg3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbxOdg3.Name = "tbxOdg3";
-            this.tbxOdg3.Size = new System.Drawing.Size(76, 20);
+            this.tbxOdg3.Size = new System.Drawing.Size(100, 22);
             this.tbxOdg3.TabIndex = 22;
             this.tbxOdg3.TextChanged += new System.EventHandler(this.tbxOdg3_TextChanged);
             // 
             // tbxOdg2
             // 
-            this.tbxOdg2.Location = new System.Drawing.Point(76, 367);
-            this.tbxOdg2.Margin = new System.Windows.Forms.Padding(2);
+            this.tbxOdg2.Location = new System.Drawing.Point(101, 452);
+            this.tbxOdg2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbxOdg2.Name = "tbxOdg2";
-            this.tbxOdg2.Size = new System.Drawing.Size(76, 20);
+            this.tbxOdg2.Size = new System.Drawing.Size(100, 22);
             this.tbxOdg2.TabIndex = 21;
             this.tbxOdg2.TextChanged += new System.EventHandler(this.tbxOdg2_TextChanged);
             // 
             // btnObrisi
             // 
             this.btnObrisi.Enabled = false;
-            this.btnObrisi.Location = new System.Drawing.Point(622, 383);
-            this.btnObrisi.Margin = new System.Windows.Forms.Padding(2);
+            this.btnObrisi.Location = new System.Drawing.Point(829, 471);
+            this.btnObrisi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnObrisi.Name = "btnObrisi";
-            this.btnObrisi.Size = new System.Drawing.Size(80, 31);
+            this.btnObrisi.Size = new System.Drawing.Size(107, 38);
             this.btnObrisi.TabIndex = 20;
             this.btnObrisi.Text = "Obrisi";
             this.btnObrisi.UseVisualStyleBackColor = true;
@@ -179,10 +175,10 @@
             // 
             // btnSveska
             // 
-            this.btnSveska.Location = new System.Drawing.Point(16, 15);
-            this.btnSveska.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSveska.Location = new System.Drawing.Point(21, 18);
+            this.btnSveska.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSveska.Name = "btnSveska";
-            this.btnSveska.Size = new System.Drawing.Size(80, 29);
+            this.btnSveska.Size = new System.Drawing.Size(107, 36);
             this.btnSveska.TabIndex = 19;
             this.btnSveska.Text = "SVESKA";
             this.btnSveska.UseVisualStyleBackColor = true;
@@ -191,10 +187,10 @@
             // pnlSveska
             // 
             this.pnlSveska.Enabled = false;
-            this.pnlSveska.Location = new System.Drawing.Point(457, 195);
-            this.pnlSveska.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlSveska.Location = new System.Drawing.Point(609, 240);
+            this.pnlSveska.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pnlSveska.Name = "pnlSveska";
-            this.pnlSveska.Size = new System.Drawing.Size(51, 35);
+            this.pnlSveska.Size = new System.Drawing.Size(68, 43);
             this.pnlSveska.TabIndex = 18;
             this.pnlSveska.Visible = false;
             this.pnlSveska.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlSveska_Paint);
@@ -204,9 +200,10 @@
             // 
             // tbxOdg1
             // 
-            this.tbxOdg1.Location = new System.Drawing.Point(76, 344);
+            this.tbxOdg1.Location = new System.Drawing.Point(101, 423);
+            this.tbxOdg1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbxOdg1.Name = "tbxOdg1";
-            this.tbxOdg1.Size = new System.Drawing.Size(76, 20);
+            this.tbxOdg1.Size = new System.Drawing.Size(100, 22);
             this.tbxOdg1.TabIndex = 17;
             this.tbxOdg1.TextChanged += new System.EventHandler(this.tbxOdg1_TextChanged);
             // 
@@ -217,19 +214,18 @@
             // 
             // lblPitanje
             // 
-            this.lblPitanje.AutoSize = true;
-            this.lblPitanje.Location = new System.Drawing.Point(288, 24);
-            this.lblPitanje.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPitanje.Location = new System.Drawing.Point(384, 30);
             this.lblPitanje.Name = "lblPitanje";
-            this.lblPitanje.Size = new System.Drawing.Size(38, 13);
+            this.lblPitanje.Size = new System.Drawing.Size(63, 20);
             this.lblPitanje.TabIndex = 32;
             this.lblPitanje.Text = "pitanje";
+            this.lblPitanje.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // frmSlobodanNivo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.lblPitanje);
             this.Controls.Add(this.btnPosalji);
             this.Controls.Add(this.pbxSrce3);
@@ -246,6 +242,7 @@
             this.Controls.Add(this.btnSveska);
             this.Controls.Add(this.pnlSveska);
             this.Controls.Add(this.tbxOdg1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmSlobodanNivo";
             this.Text = "FormaSlobodanNivocs";
             this.Load += new System.EventHandler(this.frmSlobodanNivo_Load);
